f702d2dcb1a59c396a446ee3a69d7ab374555ef2
JoshRees-Haughton <joshreeshaughton@proton.megit config --global user.name JoshRees-Haughtongit config --global user.email joshreeshaughton@proton.me>
2023-07-31 00:38:57 +0100

    Initial commit

---
+++ telephone_number_validator.js
@@ -0,0 +1,10 @@
+function telephoneCheck(str) {
+    //Check that the string is all numbers, dashes or brackets
+  
+    //Check for 10 digits
+  
+    //If not 10 digits, then 1 is the first digit
+    return true;
+  }
+  
+  telephoneCheck("555-555-5555");
